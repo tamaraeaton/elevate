28b01969defeb2b2099106ce86078e36fad0f003
akinghill <austinkinghill@gmail.com>
2021-07-01 09:04:24 -0700

    cleanup

--- App.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import {Text, View} from 'react-native';
-import Nav from './src/Navigation/navigator';
-
-function App() {
-  return <Nav />;
-}
-export default App;
-
