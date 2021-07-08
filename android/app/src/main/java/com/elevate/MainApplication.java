44f7b7108ffdcc4ed100cd9c7881df44ea4a8685
joseph-caldwell <67066124+joseph-caldwell@users.noreply.github.com>
2021-07-08 14:33:04 -0500

    navigation working
    manually link react-native-gesture-handler

--- android/app/src/main/java/com/elevate/MainApplication.java
+++ android/app/src/main/java/com/elevate/MainApplication.java
@@ -2,8 +2,13 @@ package com.elevate;
 
 import android.app.Application;
 import android.content.Context;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,28 +17,27 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
 
-  private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHost(this) {
-        @Override
-        public boolean getUseDeveloperSupport() {
-          return BuildConfig.DEBUG;
-        }
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
 
-        @Override
-        protected List<ReactPackage> getPackages() {
-          @SuppressWarnings("UnnecessaryLocalVariable")
-          List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-          return packages;
-        }
+    @Override
+    protected List<ReactPackage> getPackages() {
+      @SuppressWarnings("UnnecessaryLocalVariable")
+      List<ReactPackage> packages = new PackageList(this).getPackages();
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages;
+    }
 
-        @Override
-        protected String getJSMainModuleName() {
-          return "index";
-        }
-      };
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -43,7 +47,7 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public void onCreate() {
     super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    SoLoader.init(this, /* native exopackage */false);
     initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
@@ -55,7 +59,9 @@ public class MainApplication extends Application implements ReactApplication {
    * @param reactInstanceManager
    */
   private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
+    Context context,
+    ReactInstanceManager reactInstanceManager
+  ) {
     if (BuildConfig.DEBUG) {
       try {
         /*
@@ -64,8 +70,12 @@ public class MainApplication extends Application implements ReactApplication {
         */
         Class<?> aClass = Class.forName("com.elevate.ReactNativeFlipper");
         aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
+          .getMethod(
+            "initializeFlipper",
+            Context.class,
+            ReactInstanceManager.class
+          )
+          .invoke(null, context, reactInstanceManager);
       } catch (ClassNotFoundException e) {
         e.printStackTrace();
       } catch (NoSuchMethodException e) {
---
+++ src/components/maps/MapScreen.js
@@ -0,0 +1,153 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, Button, Alert} from 'react-native';
+import MapView, {
+  MAP_TYPES,
+  PROVIDER_DEFAULT,
+  Callout,
+  Marker,
+  PROVIDER_GOOGLE,
+} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
+export default class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userLat: 0,
+      userLon: 0,
+      latitudeDelta: 0.05,
+      longitudeDelta: 0.05,
+      desLat: 0,
+      desLon: 0,
+      userLocation: 0,
+      eleCoord: 0,
+      desEle: 0,
+      marker: null,
+      eleDiff: 0,
+    };
+  }
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+      pos => {
+        console.log(pos),
+          this.setState({
+            userLat: pos.coords.latitude,
+            userLon: pos.coords.longitude,
+            userLocation: pos,
+            
+          });
+      },
+      error => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 15000,
+        maximumAge: 10000,
+        forceRequestLocation: true,
+        fastestInterval: 5000,
+      },
+    );
+  }
+
+  render() {
+    const {userLat, userLon, desLat, desLon} = this.state;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.blueTitle}>ELEVATE</Text>
+        <MapView
+          style={styles.map}
+          showsUserLocation={true}
+          userLocationCalloutEnabled={true}
+          provider={'google'}
+          mapType="standard"
+          region={{
+            latitude: userLat,
+            longitude: userLon,
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05,
+          }}></MapView>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: 'gold',
+  },
+  map: {
+    height: 600,
+    width: 600,
+  },
+
+  blueTitle: {
+    color: 'navy',
+    fontWeight: 'bold',
+    fontSize: 25,
+    textAlign: 'center',
+    marginBottom: 10,
+    height: 30,
+    backgroundColor: 'gold',
+  },
+  button: {
+    backgroundColor: 'blue',
+    color: 'white',
+  },
+  elevateButtonSection: {
+    width: '100%',
+    height: '10%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+// import React, {Component} from 'react';
+// import {View, StyleSheet} from 'react-native';
+// import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
+
+// export default class MapScreen extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       userLatitude: 0,
+//       userLongitude: 0,
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <MapView
+//           showsUserLocation
+//           followsUserLocation
+//           provider={PROVIDER_GOOGLE}
+//           style={styles.map}
+//           region={{
+//             latitude: 37.78825,
+//             longitude: -122.4324,
+//             latitudeDelta: 0.015,
+//             longitudeDelta: 0.0121,
+//           }}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     ...StyleSheet.absoluteFillObject,
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//   },
+//   map: {
+//     ...StyleSheet.absoluteFillObject,
+//   },
+// });
